d3242b26398cd4e99a055a58c27f438e494f2b28
Kirill V. Gerasimov <kiriger@gmail.com>
2017-08-20 13:57:29 +0000

    [GCP] Added git configs for email and user name to setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -6,6 +6,8 @@ sudo apt-get install maxima
 sudo apt-get install git
 
 git clone https://github.com/admech/maxima.git --branch gcp --single-branch
+git config --global user.email "kiriger@gmail.com"
+git config --global user.name "Kirill V. Gerasimov"
 
 echo "   " >> ~/.bashrc
 echo "# a handy function for viewing simulation logs" >> ~/.bashrc
