5c5270f7b4d67a504acb3474374d51805c952eb9
Kirill V. Gerasimov <kiriger@gmail.com>
2017-11-04 09:22:23 +0000

    [AWS] Added apt-get update trying to fix gnuplot not installed.

--- src_setup_env.sh
+++ src_setup_env.sh
@@ -3,6 +3,8 @@
 # run this on a fresh env to get everything for sumulation
 # Maxima is being built from source with SBCL - the fast LISP.
 
+sudo apt-get --update
+
 sudo apt-get --yes install sbcl
 sudo apt-get --yes install make
 sudo apt-get --yes install autotools-dev
